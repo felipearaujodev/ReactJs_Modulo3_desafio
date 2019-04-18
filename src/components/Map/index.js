ef7e2e336565d6f103eba1bfb828a1648b833715
Felipe Araujo <comunicar.felipearaujo@gmail.com>
2019-04-18 13:29:35 -0300

    Desafio Final

---
+++ src/components/Map/index.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+
+import MapGL, { Marker } from "react-map-gl";
+
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Creators as ModalActions } from "../../store/ducks/modal";
+
+import "./styles.css";
+
+const TOKEN =
+  "pk.eyJ1IjoiZmVsaXBlYmFuZzMiLCJhIjoiY2p1bDJhbzYxMG9xbDQzcXY5eGZvcWNkOCJ9.H-rSqLi5KRPxEoGqd3saKw";
+
+class Map extends Component {
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: -21.975923,
+      longitude: -46.780686,
+      zoom: 15
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.resize);
+    this.resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.resize);
+  }
+
+  resize = () => {
+    const { viewport } = this.state;
+    this.setState({
+      viewport: {
+        ...viewport,
+        width: window.innerWidth,
+        height: window.innerHeight
+      }
+    });
+  };
+
+  handleMapClick = async e => {
+    const [longitude, latitude] = e.lngLat;
+    const { showModal } = this.props;
+
+    // console.log(e.lngLat);
+
+    await showModal({ latitude, longitude });
+  };
+
+  render() {
+    const { viewport: viewportState } = this.state;
+    const { users } = this.props;
+    return (
+      <MapGL
+        {...viewportState}
+        onClick={this.handleMapClick}
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxApiAccessToken={TOKEN}
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+        {users.data.map(user => (
+          <Marker
+            latitude={user.cordinates.latitude}
+            longitude={user.cordinates.longitude}
+            key={user.id}
+          >
+            <img
+              className="avatar"
+              alt={`${user.name} Avatar`}
+              src={user.avatar}
+            />
+          </Marker>
+        ))}
+      </MapGL>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  users: state.users
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(ModalActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Map);
